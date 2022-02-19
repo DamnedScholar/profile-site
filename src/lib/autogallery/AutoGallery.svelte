8a9a84bcfb90fed93e12b6370a58e30d51a6f0ea
Holland Wilson <stickfigureonfire@gmail.com>
2022-02-19 01:38:02 -0600

    Static adapter

---
+++ src/lib/autogallery/AutoGallery.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { readdir } from 'fs/promises'
+
+    import Gallery from 'svelte-image-gallery'
+
+    async function setup() {
+        let images = await readdir('static/gallery')
+        return images
+    }
+</script>
+
+<Gallery>
+    {#await setup}
+        Loading
+    {:then contents}
+        {#each contents as img}
+            <img src='{img}' alt='{img}'>
+        {/each}
+    {/await}
+</Gallery>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,15 +7,20 @@
 
 	import FaBicycle from 'svelte-icons/fa/FaBicycle.svelte'
 	import FaInstagram from 'svelte-icons/fa/FaInstagram.svelte'
+	import TiMail from 'svelte-icons/ti/TiMail.svelte'
 
-	import Gallery from 'svelte-gallery';
+	import AutoGallery from '$lib/autogallery/AutoGallery.svelte'
 
-	const images = [
-		{ src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-		{ src: 'https://source.unsplash.com/random', width: 400, height: 600 },
-		{ src: 'https://source.unsplash.com/random', width: 800, height: 1200 },
-		{ src: 'https://source.unsplash.com/random', width: 300, height: 200 },
-	];
+	let images = [
+		{
+			'src': 'static/amwaffles.jpg',
+			'alt': '',
+		},
+		{
+			'src': '',
+			'alt': '',
+		},
+	]
 </script>
 
 
@@ -24,44 +29,50 @@
 </svelte:head>
 
 <section id="main">
-	<h1>
-		Holland Wilson, Chevalier de Cuisine
-	</h1>
+	<header>
+		<h1 id="headline">
+			Holland Wilson<br /><span class="subtitle">Chevalier de Cuisine</span>
+		</h1>
+		<img id="headshot" src="static/headshot.png" alt="A picture of me at Caroline's in Joplin.">
+	</header>
 
-	<section id="contact">
+	<article id="bio">
+		<section id="contact">
+			<h2>
+				Contact Info
+			</h2>
+			
+			<p class="address">
+				<label for="insta" class="icon"><TiMail /></label> <a href="mailto:ChefHolland@eclectic-co.com">ChefHolland@eclectic-co.com</a>
+			</p>
+			<p class="address">
+				<label for="insta" class="icon"><FaInstagram /></label> <a href="https://instagram.com/damned_scholar" id="insta" target="_blank" rel="noopener noreferrer">@damned_scholar</a>
+			</p>
+		</section>
+	
+		<section id="about">
+			<h2>
+				My Journey
+			</h2>
+	
+			<p>Somehow, I've wound up transmuting a nerd with too many hobbies into a multi-niche foodservice and hospitality junkie. I've had the privilege to learn from some of Springfield, MO's finest restaurants and am in the habit of acquiring multiple new skills a year. I have developed and sold vegan cheese, tended multiple bars, manufactured high-quality desserts to exacting specifications, single-handedly catered events, and engaged in publicity events to promote my work community on Commercial Street.</p>
+		</section>
+	</article>
+
+	<article id="cv">
 		<h2>
-			Contact Info
-		</h2>
-		
-		<label for="insta"><FaInstagram /></label> <a href="https://instagram.com/damnedscholar" target="_blank" rel="noopener noreferrer">@damnedscholar</a>
-	</section>
-
-	<section id="about">
-		<h2>
-			My Journey
+			Looking for something special?
 		</h2>
 
-		<p>Somehow, I've wound up transmuting a nerd with too many hobbies into a multi-niche foodservice and hospitality junkie. I've had the privilege to learn from some of Springfield, MO's finest restaurants and am in the habit of acquiring multiple new skills a year. I have developed and sold vegan cheese, tended multiple bars, manufactured high-quality desserts to exacting specifications, single-handedly catered events, and engaged in publicity events to promote my work community on Commercial Street.</p>
-	</section>
+		<p>If you have a demanding event concept but don't want to put too much work into planning the food, a new foodservice concept that needs methodical R&D, a class that could benefit from a hands-on lecture about science topics, or similar projects, let's talk. I'm an experienced event chef and available as a freelance consultant.</p>
+	</article>
 
 	<section id="gallery">
-		<Gallery {images} />
+		<AutoGallery />
 	</section>
 </section>
 
 <style>
-	:root {
-		--text: #06254B;
-		--text-accent: #27235D;
-		--text-head: #0F0B50;
-		--frame-bg: #1D3957;
-		--content-bg: #CFC5B4;
-		--content-border-inner: #9A7E4C;
-		--content-border-outer: #734B00;
-	}
-	body {
-		background-color: var(--bg-frame);
-	}
 	section {
 		display: flex;
 		flex-direction: column;
@@ -73,27 +84,81 @@
 
 	h1 {
 		width: 100%;
-		color: var(--text-head)
+		color: var(--heading-color);
+		margin: 5px;
 	}
 	h2 {
+		font-size: 1.4rem;
 		width: 100%;
-		color: var(--text-head)
+		color: var(--heading-color);
+		margin: 5px;
 	}
 	h3 {
 		width: 100%;
-		color: var(--text-head)
+		color: var(--heading-color);
+		margin: 5px;
 	}
 	h4 {
 		width: 100%;
-		color: var(--text-head)
+		color: var(--heading-color);
+		margin: 5px;
 	}
 	h5 {
 		width: 100%;
-		color: var(--text-head)
+		color: var(--heading-color);
+		margin: 5px;
 	}
 
 	#main {
-		background-color: #E4D6A8;
+		background-color: #E6E2DB;
+		box-shadow: 4px 5px 10px 5px rgba(154,126,76,0.55);
+		border-radius: 8px;
+		padding: 10px;
+		margin-top: 120px;
+	}
+
+	#main header {
+		display: flex;
+		align-items: flex-end;
+		height: 100px;
+		width: 100%;
+		overflow: visible;
+	}
+
+	#headline {
+		display: inline-block;
+	}
+	#headshot {
+		position: relative;
+		bottom: -40px;
+	}
+
+	#bio {
+		display: flex;
+		align-items: flex-start;
+	}
+
+	.address {
+		display: grid;
+		grid-template-columns: 20px 270px;
+		grid-template-rows: repeat(2, auto);
+		grid-column-gap: 4px;
+		grid-row-gap: 4px;
+		margin-bottom: 0;
+	}
+
+	#about {
+		flex-grow: 2;
+	}
+
+	.subtitle {
+		font-size: 80%;
+		font-style: italic;
+	}
+
+	.icon {
+		width: 16px;
+		height: 16px;
 	}
 
 </style>
